f377a6729f0f8e76cf2a530c39def0ca1e70f277
nguyenmanhhieu <hieunm14092001@gmail.com>
2022-06-03 22:35:59 +0700

    nguyen am nguyen duong
    nguyen manh hieu

---
+++ so nguyen am nguyen duong.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  so nguyen am nguyen duong nguyen manh hieu
+//
+//  Created by Hiếu Hee on 03/06/2022.
+//
+
+#include <stdio.h>
+int main(void)
+{
+    int x;
+        printf("nhap so\n");
+        scanf("%d",&x);
+    if (x>0)
+        printf("la so nguyen duong\n");
+    if (x<0)
+    printf("la so nguyen am\n");
+    if (x==0)
+    printf("so khong am khong duong");
+    return 0;
+}
+ 
